afaeee69a941dcc8e4840ae3ddee89ab4f742f08
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-12 18:21:31 +0530

    Add validation for registration

--- src/Validate.java
+++ src/Validate.java
@@ -3,21 +3,38 @@ import java.util.Map;
 
 public class Validate {
 
-    public static Boolean validateUser(User user)
+    public static Boolean validateUser(String phone)
     {
         Map<String,User> users =  UserDatabase.getUsers();
-        if(users.containsKey(user.getPhoneNumber()))
+        if(users.containsKey(phone))
             return true;
         else
             return false;
     }
 
-    public static Boolean validateInput(String inputString)
+    public static Boolean validateAction(String action)
     {
-        if(inputString.length()>1 || inputString.isEmpty())
+        if(action.length()>1 || action.isEmpty())
             return false;
-        else if(inputString.charAt(0)<='0' && inputString.charAt(0)>'9')
+        else if(action.charAt(0)<='0' && action.charAt(0)>'9')
             return false;
+        return true;
+    }
+
+    public static Boolean validateRegistration(String newUser){
+        System.out.println(newUser);
+        String []userDetails = newUser.split(",");
+        System.out.println(userDetails.length);
+        if(userDetails.length != 2)
+            return false;
+        if(userDetails[1].length() != 10)
+            return false;
+
+        if(Validate.validateUser(userDetails[1])){
+            System.out.println("Some user have already registered with the phone number please try with new phone number");
+        }
+
+        UserDatabase.addUserInDatabase(new User(userDetails[0],userDetails[1]));
         return true;
     }
 
