fb7611dc0b6b00dc860fb30ecaaa98c3edf13a82
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-13 09:37:01 +0530

    Add validation method for expense users list

--- src/InputValidator.java
+++ src/InputValidator.java
@@ -1,6 +1,6 @@
 public class InputValidator implements Validator{
-    public Boolean validate(String action)
-    {
+
+    public Boolean validate(String action){
         if(action.length()>1 || action.isEmpty())
             return false;
         else if(action.charAt(0)<='0' && action.charAt(0)>'9')
@@ -8,16 +8,29 @@ public class InputValidator implements Validator{
         return true;
     }
 
-    public Boolean validateDesc(String desc)
-    {
+    public Boolean validateDesc(String desc) {
         if(desc.length()>0)
             return true;
         return false;
     }
 
-    public Boolean validateCost(Double cost)
-    {
+    public Boolean validateCost(Double cost) {
         return cost>=0 ? true : false;
+    }
+
+    public Boolean validateExpenseUsers(String expenseUsers){
+        if(expenseUsers.length() == 0)
+            return false;
+        String []userIds = expenseUsers.split(",");
+        boolean flag = true;
+        for(String u: userIds){
+            User user = UserDatabase.findUserById(Integer.parseInt(u));
+            if(user == null){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
 
 }
